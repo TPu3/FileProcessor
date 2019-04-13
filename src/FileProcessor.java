05ab6c726225f55b6425bcc1f89a0c9170d3efc5
TPu3 <46763102+TPu3@users.noreply.github.com>
2019-04-13 17:39:15 +0300

    improve

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -3,6 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
