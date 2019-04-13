b313b98a497b6ff2ba9e663ae8e7e5a1e53b3d54
TPu3 <46763102+TPu3@users.noreply.github.com>
2019-04-13 15:11:22 +0300

    test

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -1,8 +1,10 @@
 import javax.imageio.IIOException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class FileProcessor {
@@ -10,7 +12,7 @@ public class FileProcessor {
         FileInputStream fin= null;
         int i;
         StringBuilder word = new StringBuilder();
-        ArrayList<String> words=null;
+        ArrayList<String> words = new ArrayList<String>();
         try {
             fin = new FileInputStream(fileName);
         }catch (FileNotFoundException e) {
@@ -20,25 +22,37 @@ public class FileProcessor {
             do {
                 i = fin.read();
                 if ( i != -1 ) {
-                    int count = 0;
+
                     char reader = (char)(i);
                     if (Character.isLetter(reader)){
                      word.append(reader);
-                     count++;
+
                     }
-                    if (reader=='\'') {
-                        char temp =(char)fin.read();
-                        if (Character.isLetter(temp)) {
+                   /* if (reader=='\'') {
+                        i =(char)fin.read();
+                        if (Character.isLetter(i)) {
                             word.append('\'');
-                            word.append(temp);
+                           // word.append(temp);
+                        }
+                    }*/
+                    if ((Character.isSpaceChar(reader))||!(Character.isLetter(reader))) {
+                        if (reader == '\'') {
+                            char temp = (char) fin.read();
+                            if (Character.isLetter(temp)) {
+                                word.append(reader);
+                                word.append(temp);
+                            }
+                        } else {
+                            // System.out.println(word.toString());
+                            if ((words.indexOf(word.toString().toLowerCase())) == -1) {
+                                words.add(word.toString().toLowerCase());
+                                word.delete(0, word.length());
+                            }
                         }
                     }
-                    if ((Character.isSpaceChar(reader))&&(count>0)){
-                        words.add(word.toString().toLowerCase());
-                    }
-
                 }
             } while ( i != - 1 );
+            words.add(word.toString().toLowerCase());
             } catch (IOException e) {
             System.out.println(e.getMessage());
         } try{
@@ -48,4 +62,20 @@ public class FileProcessor {
       return words;
     }
 
+
+
+    private static void writeFile (String [] ar, String name) {
+        FileOutputStream fout = null;
+        try {
+            fout = new FileOutputStream(name);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+       Arrays.sort(readFile(args[0]).toArray());
+    }
 }
